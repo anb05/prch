75aa9c9dbcdf484ffdd3eca2d24d76333377066f
alexandr05 <alexandr05@list.ru>
2019-12-22 22:13:19 +0200

    added ProjectChooserList class

---
+++ projectlistwidget.cpp
@@ -0,0 +1,37 @@
+#include "projectlistwidget.hpp"
+
+namespace prch {
+
+ProjectListWidget::ProjectListWidget(QWidget* parent)
+    : QListWidget(parent)
+{}
+
+ProjectListWidget::~ProjectListWidget()
+{}
+
+void ProjectListWidget::mousePressEvent(QMouseEvent* event)
+{
+
+}
+
+void ProjectListWidget::mouseMoveEvent(QMouseEvent* event)
+{
+
+}
+
+void ProjectListWidget::dragEnterEvent(QDragEnterEvent* event)
+{
+
+}
+
+void ProjectListWidget::dragMoveEvent(QDragMoveEvent* event)
+{
+
+}
+
+void ProjectListWidget::dropEvent(QDropEvent* event)
+{
+
+}
+
+} // namespace prch
---
+++ projectlistwidget.hpp
@@ -0,0 +1,44 @@
+#ifndef PROJECTLISTWIDGET_HPP
+#define PROJECTLISTWIDGET_HPP
+
+#include <QListWidget>
+#include <QPoint>
+
+class QMouseEvent;
+class QDragEnterEvent;
+class QDragMoveEvent;
+class QDropEvent;
+
+namespace prch {
+
+class ProjectListWidget : public QListWidget
+{
+    Q_OBJECT
+public:
+    explicit ProjectListWidget(QWidget* parent = nullptr);
+    ~ProjectListWidget() override;
+
+    explicit ProjectListWidget(const ProjectListWidget& ) = delete;
+    explicit ProjectListWidget(ProjectListWidget&& )      = delete;
+
+public:
+    ProjectListWidget& operator= (const ProjectListWidget& ) = delete;
+    ProjectListWidget& operator= (ProjectListWidget&& )      = delete;
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void dragEnterEvent(QDragEnterEvent* event) override;
+    void dragMoveEvent(QDragMoveEvent* event) override;
+    void dropEvent(QDropEvent* event) override;
+
+private:
+    void performDrag();
+
+private:
+    QPoint _starPos;
+};
+
+} // namespace prch
+
+#endif // PROJECTLISTWIDGET_HPP
